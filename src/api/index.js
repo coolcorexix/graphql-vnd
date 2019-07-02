19aa1aa4d93830a060952ff3f04ee1e81da5b048
chipueatfast <15520604@gm.uit.edu.vn>
2019-07-02 14:12:55 +0700

    boiler plate for express and graphql

---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+import Express from 'express';
+import bodyParser from 'body-parser';
+import GraphQLEndpoint from './graphql';
+
+
+const app = Express();
+
+// parse incoming request with JSON body
+app.use(bodyParser.json());
+
+// avoid CORS blocked
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS, PATCH")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if (req.method === "OPTIONS" || req.method === "OPTIONS") {
+        res.sendStatus(200);
+        return;
+    }
+    next();
+});
+
+app.use('/graphql', GraphQLEndpoint);
+
+app.listen(process.env.PORT);
+console.log(`Call me on the hotline bling ${process.env.PORT}`);
---
+++ src/app/start.js
@@ -0,0 +1,9 @@
+require("@babel/polyfill");
+
+require('@babel/register')({
+    presets: ['@babel/preset-env'],
+});
+
+require('dotenv').config();
+
+module.exports = require('../api');
