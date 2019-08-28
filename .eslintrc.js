47f4b7e867b1ffd8133248913bb3adfd49f12610
chipueatfast <15520604@gm.uit.edu.vn>
2019-08-28 18:37:44 +0700

    update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,16 +10,35 @@ module.exports = {
     'Atomics': 'readonly',
     'SharedArrayBuffer': 'readonly',
   },
+  'plugins':
+    ['import'],
   'parserOptions': {
     'ecmaVersion': 2018,
     'sourceType': 'module',
   },
   'rules': {
-    "new-cap": 0,
-    "require-jsdoc": 0,
-    "no-undef": 1,
+    'import/no-unresolved': [2, {commonjs: true, amd: true}],
+    'new-cap': 0,
+    'require-jsdoc': 0,
+    'import/named': 2,
+    'no-undef': 2,
     'indent': [2, 4, {
       'SwitchCase': 1
     }],
   },
+  'settings': {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['mapper-gql', './src/api/graphql'],
+          ['mapper-api', './src/api'],
+          ['~', './src']
+        ],
+        extensions: ['.ts', '.js', '.json'],
+      }
+    },
+    'flowtype': {
+      'onlyFilesWithFlowAnnotation': true
+    }
+  }
 };
